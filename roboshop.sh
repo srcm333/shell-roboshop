1ba50b8412c05146740eeedbc49a6fd0d8d06cbd
srcm333 <srcm333@gmail.com>
2026-05-27 16:07:51 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z07343973IZWQHPNL7C9J" # replace with your zone ID
+DOMAIN_NAME="devpreactice.online" # replace with your domain name
+
+for instance in $@
+do
+    echo "Launching instance: $instance"
+    INSTANCE_id=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+	      --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]' \
+	      --query 'Instances[0].InstanceId' \
+        --output text
+    )
+    echo Instance ID: $INSTANCE_ID"
+
+    if [ $instance == "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $NSTANCE_id \ 
+           --query 'Reservations[*].Instances[*].PublicIpAddress' \ 
+           --output text
+        )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $NSTANCE_id \ 
+          --query 'Reservations[*].Instances[*].PrivateIpAddress' \ 
+          --output text
+        )
+        R53_RECORD="$instance.$DOMAIN_NAME"
+    fi  
+    
+done
+ 
