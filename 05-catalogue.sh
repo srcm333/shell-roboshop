21408ca7714c4cec3805b740ba03ad6f2bafefa4
srcm333 <srcm333@gmail.com>
2026-05-27 20:09:18 +0530

    roboshop

--- 05-catalogue.sh
+++ 05-catalogue.sh
@@ -38,9 +38,37 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-    echo "system user roboshop already created ... $Y SKIPPING &N"
+    echo -e "system user roboshop already created ... $Y SKIPPING &N"
 fi    
 
-mkdir -p /app &>>LOGS_FILE
+rm -rf /app
+VALIDATE $? "Removing exesting code"
+
+rm -rf /tmp/catalogue.zip
+VALIDATE $? "Removed catalogue zip"
+
+mkdir -p /app &>>$LOGS_FILE
 VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
+cd /app 
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
+VALIDATE $? "Downloaded and extracted catalogue code"
+
+npm install &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogeu.service
+VALIDATE $? "Created systemctl service"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Added Mongo repo" 
+
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+VALIDATE $? "Installed MongoDB client"
+
+
+
+
+
 
