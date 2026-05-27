90df4c3298ae97e3a89f0969b9364a66c1be73cd
srcm333 <srcm333@gmail.com>
2026-05-27 16:50:51 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,51 +6,54 @@ DOMAIN_NAME="devpreactice.online" # replace with your domain name
 
 for instance in $@
 do
-  echo "Launching instance: $instance"
-  INSTANCE_id=$(aws ec2 run-instances \
-      --image-id ami-0220d79f3f480ecf5 \
-      --instance-type t3.micro \
-      --security-groups "roboshop-common" "roboshop-$instance" \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
-      --query 'Instances[0].InstanceId' \
-      --output text
-  )
-  echo Instance ID: $INSTANCE_ID"
-  if [ $instance == "frontend" ]; then
-      IP=$(aws ec2 describe-instances --instance-ids $NSTANCE_id \ 
-         --query 'Reservations[*].Instances[*].PublicIpAddress' \ 
-         --output text
-      )
-      R53_RECORD="$DOMAIN_NAME"
-  else
-      IP=$(aws ec2 describe-instances --instance-ids $NSTANCE_id \ 
-        --query 'Reservations[*].Instances[*].PrivateIpAddress' \ 
+    echo "Launching instance: $instance"
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id ami-0220d79f3f480ecf5 \
+        --instance-type t3.micro \
+        --security-groups "roboshop-common" "roboshop-$instance" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+        --query 'Instances[0].InstanceId' \
         --output text
-      )
-      R53_RECORD="$instance.$DOMAIN_NAME"
+    )
+    echo "Instance ID: $INSTANCE_ID"
+
+    if [ $instance == "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PublicIpAddress' \
+         --output text
+        )
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+         --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+         --output text
+        )
+        R53_RECORD="$instance.$DOMAIN_NAME"
     fi
-  #### Updating R53 Record ####
-  aws route53 change-resource-record-sets \
-  --hosted-zone-id YOUR_HOSTED_ZONE_ID \
-  --change-batch '
-    {
-  "comment": "update A record to new IP",
-    "change":[
-      {
-        "Action": "UPSERT",
-        "ResourceRecordSet": {
-           "Name": "'$R53_RECORD'",
-           "Type": "A",
-           "TTL": 1,
-           "ResourceRecords": [
-             {
-               "Value": "'$IP'"
-              }
+    
+    #### Updating R53 Record ####
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+        {
+            "Comment": "Update A record to new IP",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "'$R53_RECORD'",
+                        "Type": "A",
+                        "TTL": 1,
+                        "ResourceRecords": [
+                            {
+                                "Value": "'$IP'"
+                            }
+                        ]
+                    }
+                }
             ]
-          }
         }
-      ]
-    }
-  '
+    '
+
 done
  
