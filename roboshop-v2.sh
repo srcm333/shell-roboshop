02c316400e3a68dcef5adc942513c003a84ee784
srcm333 <srcm333@gmail.com>
2026-05-28 22:44:07 +0530

    roboshop

---
+++ roboshop-v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z07343973IZWQHPNL7C9J" # replace with your zone ID
+DOMAIN_NAME="devpreactice.online" # replace with your domain name
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+###Validation ###
+if [ $# -lt 2 ]; then
+     echo -e "$R ERROR:: Atleast 2 arguments required $N"
+     echo "USAGE: $0 [ create/delete ] [instance1] [instance2...]"
+     exit 1
+fi
+
