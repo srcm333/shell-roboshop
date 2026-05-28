28ba92e0dbeeedd48340ecf1e1653f5bc84d9e1a
srcm333 <srcm333@gmail.com>
2026-05-28 23:34:25 +0530

    roboshop

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -24,6 +24,11 @@ if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
      exit 1
 fi
 
+get_instance_id(){
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop-$name" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text
+}
+
 for instance in $@
 do
     INSTANCE_ID=$(get_instance_id $instance)
